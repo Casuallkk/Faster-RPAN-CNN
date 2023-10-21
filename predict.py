3a5857cf64152e8cf2a0352921f93dbfee252dff
LeiXinyue <casuallkk@gamil.com>
2023-10-21 21:43:15 +0800

    修改了predict，跑通了

--- predict.py
+++ predict.py
@@ -9,11 +9,7 @@ from frcnn import FRCNN
 
 if __name__ == "__main__":
     frcnn = FRCNN()
-    #   'predict'           表示单张图片预测，如果想对预测过程进行修改，如保存图片，截取对象等，可以先看下方详细的注释
-    #   'dir_predict'       表示遍历文件夹进行检测并保存。默认遍历img文件夹，保存img_out文件夹，详情查看下方注释。
     mode = "predict"
-    #   crop                指定了是否在单张图片预测后对目标进行截取
-    #   count               指定了是否进行目标的计数
     #   crop、count仅在mode='predict'时有效
     crop = False
     count = False
