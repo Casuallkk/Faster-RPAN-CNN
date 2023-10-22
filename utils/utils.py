d41041b004b7bf183f95ae2c7e84ea15b1a9696a
LeiXinyue <casuallkk@gamil.com>
2023-10-22 13:43:33 +0800

    修改了utils

--- utils/utils.py
+++ utils/utils.py
@@ -5,10 +5,8 @@ import torch
 from PIL import Image
 
 
-# ---------------------------------------------------------#
 #   将图像转换成RGB图像，防止灰度图在预测时报错。
 #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-# ---------------------------------------------------------#
 def cvtColor(image):
     if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
         return image
@@ -16,20 +14,15 @@ def cvtColor(image):
         image = image.convert('RGB')
         return image
 
-    # ---------------------------------------------------#
-
 
 #   对输入图像进行resize
-# ---------------------------------------------------#
 def resize_image(image, size):
     w, h = size
     new_image = image.resize((w, h), Image.BICUBIC)
     return new_image
 
 
-# ---------------------------------------------------#
 #   获得类
-# ---------------------------------------------------#
 def get_classes(classes_path):
     with open(classes_path, encoding='utf-8') as f:
         class_names = f.readlines()
@@ -37,17 +30,13 @@ def get_classes(classes_path):
     return class_names, len(class_names)
 
 
-# ---------------------------------------------------#
 #   获得学习率
-# ---------------------------------------------------#
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
 
-# ---------------------------------------------------#
 #   设置种子
-# ---------------------------------------------------#
 def seed_everything(seed=11):
     random.seed(seed)
     np.random.seed(seed)
@@ -58,9 +47,7 @@ def seed_everything(seed=11):
     torch.backends.cudnn.benchmark = False
 
 
-# ---------------------------------------------------#
 #   设置Dataloader的种子
-# ---------------------------------------------------#
 def worker_init_fn(worker_id, rank, seed):
     worker_seed = rank + seed
     random.seed(worker_seed)
